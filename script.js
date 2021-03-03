0578be93bba7cac4ca8fae923ff98bed2a5ff8a2
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-03-03 02:40:46 -0800

    i have started adding js functions

---
+++ script.js
@@ -0,0 +1,8 @@
+//AUTHOR :EDSON MAGOMBO 
+//PHONE #:0881955791
+
+//alert("hello eddie");
+//console.log("hello js programmer kkk ");
+
+//LETS CREATE FUNCTIONS FOR CALCULATING DAYS FOR EVERY AGE YOU ENTER;
+var birthYear = prompt("What year were you born??");
