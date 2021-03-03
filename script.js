b61fedc77af8a911439dc6c2ebcaa78ffc978d8f
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-03-03 05:59:30 -0800

    adding some functionality

--- script.js
+++ script.js
@@ -5,4 +5,13 @@
 //console.log("hello js programmer kkk ");
 
 //LETS CREATE FUNCTIONS FOR CALCULATING DAYS FOR EVERY AGE YOU ENTER;
-var birthYear = prompt("What year were you born??");
+function ageInDays() {
+var birthYear = prompt("WHAT YEAR WERE YOU BORN??");
+var ageInDayss= ( 2021 - birthYear) * 365;
+var h1 = document.createElement("h1");
+var textAnswer = document.createTextNode("You're " +ageInDayss+ "days old");
+h1.setAttribute('id' , 'ageInDays ');
+h1.appendChild(textAnswer);
+document.getElementById("results").appendChild(h1);
+ 
+}
