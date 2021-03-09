3c44af0c7f0a37e372a6dcb0092209647e2400e8
edson-magombo <bed-com-33-18@cc.ac.mw>
2021-03-09 12:53:49 -0800

    finalised work in step1

--- script.js
+++ script.js
@@ -6,12 +6,15 @@
 
 //LETS CREATE FUNCTIONS FOR CALCULATING DAYS FOR EVERY AGE YOU ENTER;
 function ageInDays() {
-var birthYear = prompt("WHAT YEAR WERE YOU BORN??");
-var ageInDayss= ( 2021 - birthYear) * 365;
-var h1 = document.createElement("h1");
-var textAnswer = document.createTextNode("You're " +ageInDayss+ "days old");
-h1.setAttribute('id' , 'ageInDays ');
+var birthYear = prompt('WHAT YEAR WERE YOU BORN??');
+var days= ( 2021 - birthYear) * 365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode("You're " + days +" days old.");
+h1.setAttribute('id', 'ageInDays');
 h1.appendChild(textAnswer);
-document.getElementById("results").appendChild(h1);
- console.log(textAnswer);
+document.getElementById('results').appendChild(h1);
+ console.log(days);
+}
+function reset(){
+    document.getElementById('results').remove();
 }
